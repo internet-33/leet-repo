bb82255d48b73d276d699dca5c3d87f5b08c0ad6
internet-33 <interpres33@gmail.com>
2025-03-27 10:54:49 -0400

    Time: 27 ms (64.06%) Space: 30.1 MB (50.02%)

--- 0287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,22 +1,17 @@
-class Solution(object):
-    def findDuplicate(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
         slow = fast = nums[0]
 
         while True:
             slow = nums[slow]
             fast = nums[nums[fast]]
-
             if slow == fast:
                 break
-        
-        slow = nums[0]
-        
-        while slow != fast:
+
+        slow2 = nums[0]
+        while slow2 != slow:
+            slow2 = nums[slow2]
             slow = nums[slow]
-            fast = nums[fast]
-            
+
+
         return slow
